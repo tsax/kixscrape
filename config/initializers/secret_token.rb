39b5349628e3b4fe6fa74c729acfa45fd6843ce5
tsax <tsaxena86@gmail.com>
2012-07-19 19:03:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kixscrape::Application.config.secret_token = 'c5e342a33afa0b2026df8bd87fc7bb5cdc125a7bc34b1f1d6005b5a53530de09b4b09f937ca6d7a22497727b91fb76563d060abc405547d40b492e94440f1524'
